c2367aaaddb675a9b72e25366cca0e1e58d9e86b
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-11-07 00:06:10 +0530

    Started data structure module.

---
+++ Basic Data Types/List Comprehensions.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = int(input("Enter the value of x: "))
+    y = int(input("Enter the value of y: "))
+    z = int(input("Enter the value of z: "))
+    n = int(input("Enter the value of n: "))
+    
+    print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != 0])
